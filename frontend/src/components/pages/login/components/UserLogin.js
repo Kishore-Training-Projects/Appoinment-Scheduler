c32c47391a8b1d662e3b4b06bbe87c5506f5515d
Kishore D <kishoredurai7@gmail.com>
2023-06-30 23:19:34 +0530

    UPDATE . font updated

--- frontend/src/components/pages/login/components/UserLogin.js
+++ frontend/src/components/pages/login/components/UserLogin.js
@@ -105,7 +105,7 @@ export const UserLogin = () => {
                 onChange={(e) => handleChange(e)}
                 className="mx-1 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 placeholder="Enter Number"
-                required=""
+                required
                 disabled={otpsent}
               />
             </div>
